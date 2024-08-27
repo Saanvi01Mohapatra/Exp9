e2e2392ce1091f10b013cddc47e9b2167a45a01e
Saanvi01Mohapatra <saanvimohapatra05@gmail.com>
2024-08-28 01:18:49 +0530

    Add files via upload

---
+++ c9_1_2.cpp
@@ -0,0 +1,27 @@
+// Saanvi 
+// 23070123110
+// ENTC B2 
+// experiment 9
+
+#include <bits/stdc++.h> 
+using namespace std;
+void geeks()
+{
+    int var = 5;
+
+    int* ptr; 
+
+    ptr = &var;
+
+    cout<<"Value at ptr = "<<ptr<<"\n";
+    cout<<"Value at var = " <<var<<"\n";
+    cout<<"Value at *ptr = "<<*ptr<<"\n";
+
+}
+
+
+int main()
+{
+    geeks();
+    return 0;
+} 
